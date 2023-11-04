56db68c55707259be56447e94f2ce571dfea3c76
tanjil hoassain <tanjilt46@gmail.com>
2023-11-04 09:54:28 -0700

    footer section end

--- src/components/HomePages.jsx/Home.jsx
+++ src/components/HomePages.jsx/Home.jsx
@@ -1,6 +1,7 @@
 import Banner from "./Banner";
 import Faq from "./Faq";
 import Feature from "./Feature";
+import Footer from "./Footer";
 
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <Feature></Feature>
             <Faq></Faq>
+            <Footer></Footer>
             
         </div>
     );
